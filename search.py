882f2caf612193de05e8bf38bb0d9e3300787002
Ahmed ElGamal <gamoool.95@gmail.com>
2017-11-23 17:04:28 +0200

    Remove redundent code

--- search.py
+++ search.py
@@ -82,44 +82,6 @@ def tinyMazeSearch(problem):
     return  [s, s, w, s, w, w, s, w]
 
 
-def graphSearch(problem, frontier):
-    print "Start:", problem.getStartState()
-    print "Is the start a goal?", problem.isGoalState(problem.getStartState())
-    print "Start's successors:", problem.getSuccessors(problem.getStartState())
-
-    explored = []
-    frontier.push([(problem.getStartState(), "Stop", 0)])
-
-    while not frontier.isEmpty():
-        # print "frontier: ", frontier.heap
-        path = frontier.pop()
-        print "path len: ", len(path)
-        print "path: ", path
-
-        s = path[len(path) - 1]
-        s = s[0]
-        # print "s: ", s
-        if problem.isGoalState(s):
-            # print "FOUND SOLUTION: ", [x[1] for x in path]
-            return [x[1] for x in path][1:]
-
-        if s not in explored:
-            explored.append(s)
-            # print "EXPLORING: ", s
-
-            for successor in problem.getSuccessors(s):
-                # print "SUCCESSOR: ", successor
-                if successor[0] not in explored:
-                    successorPath = path[:]
-                    successorPath.append(successor)
-                    # print "successorPath: ", successorPath
-                    frontier.push(successorPath)
-                    # else:
-                    # print successor[0], " IS ALREADY EXPLORED!!"
-
-    return []
-
-
 def firstSearchSolution(problem, frontier):
     s = frontier
     start_state = problem.getStartState()
