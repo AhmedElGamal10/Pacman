2d4270920c2e6608f49f331dc463c7635b88c121
Ahmed ElGamal <gamoool.95@gmail.com>
2017-11-23 20:47:24 +0200

    Introduce cost to bfs code

--- search.py
+++ search.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -32,7 +32,6 @@ class SearchProblem:
     """
     This class outlines the structure of a search problem, but doesn't implement
     any of the methods (in object-oriented terminology: an abstract class).
-
     You do not need to change anything in this class, ever.
     """
 
@@ -45,7 +44,6 @@ class SearchProblem:
     def isGoalState(self, state):
         """
           state: Search state
-
         Returns True if and only if the state is a valid goal state.
         """
         util.raiseNotDefined()
@@ -53,7 +51,6 @@ class SearchProblem:
     def getSuccessors(self, state):
         """
           state: Search state
-
         For a given state, this should return a list of triples, (successor,
         action, stepCost), where 'successor' is a successor to the current
         state, 'action' is the action required to get there, and 'stepCost' is
@@ -64,7 +61,6 @@ class SearchProblem:
     def getCostOfActions(self, actions):
         """
          actions: A list of actions to take
-
         This method returns the total cost of a particular sequence of actions.
         The sequence must be composed of legal moves.
         """
@@ -82,7 +78,7 @@ def tinyMazeSearch(problem):
     return  [s, s, w, s, w, w, s, w]
 
 
-def firstSearchSolution(problem, frontier):
+def firstSearchSolution(problem, frontier, name):
     s = frontier
     start_state = problem.getStartState()
 
@@ -91,17 +87,21 @@ def firstSearchSolution(problem, frontier):
     parentStateMap = dict()
     parentActionMap = dict()
 
-    print start_state
+    # print start_state
 
     if problem.isGoalState(start_state):
         return solution
+    if name=="dfs" or name=="bfs":
 
-    s.push([start_state, STOP])
+        s.push([start_state, STOP, 0])
+    elif name =="ucs" or name =="astar":
+            s.push([start_state,STOP,0],0 )
 
     while not s.isEmpty():
         current = s.pop()
         current_state = current[0]
         current_action = current[1]
+        current_cost = current[2]
 
         parentActionMap[current_state] = current_action
 
@@ -117,27 +117,29 @@ def firstSearchSolution(problem, frontier):
 
             visited.append(current_state)
             successors = problem.getSuccessors(current_state)
-            print "successors are ", successors
+            # print "successors are ", successors
             for successor in successors:
-                if successor not in s and successor[0] not in visited:
-                    print "current_state ", current_state, "successor", successor[0], "current_action", current_action
-                    s.push([successor[0], successor[1]])
+                if successor[0] not in visited:
+                    # print "current_state ", current_state, "successor", successor[0], "current_action", current_action
+                    if name=="dfs" or name=="bfs":
+                        s.push([successor[0], successor[1], current_cost + 1])
+                    elif name =="ucs":
+                        s.push([successor[0], successor[1]], successor[2])
+                    elif name == "astar":
+                        s.push([successor[0], successor[1], successor[2]], successor[2] + nullHeuristic(successor[0], problem))
                     parentStateMap[successor[0]] = current_state
 
     return []
 
 
 def depthFirstSearch(problem):
-    return firstSearchSolution(problem, Stack())
+    return firstSearchSolution(problem, Stack(), "dfs")
     """
     Search the deepest nodes in the search tree first.
-
     Your search algorithm needs to return a list of actions that reaches the
     goal. Make sure to implement a graph search algorithm.
-
     To get started, you might want to try some of these simple commands to
     understand the search problem that is being passed in:
-
     print "Start:", problem.getStartState()
     print "Is the start a goal?", problem.isGoalState(problem.getStartState())
     print "Start's successors:", problem.getSuccessors(problem.getStartState())
@@ -146,7 +148,9 @@ def depthFirstSearch(problem):
     util.raiseNotDefined()
 
 def breadthFirstSearch(problem):
-    return firstSearchSolution(problem, Queue())
+    return firstSearchSolution(problem, Queue(), "bfs")
+
+
 
     """Search the shallowest nodes in the search tree first."""
     "*** YOUR CODE HERE ***"
@@ -154,7 +158,8 @@ def breadthFirstSearch(problem):
 
 def uniformCostSearch(problem):
     """Search the node of least total cost first."""
-    "*** YOUR CODE HERE ***"
+    from util import PriorityQueue
+    return firstSearchSolution(problem, PriorityQueue(), "ucs")
     util.raiseNotDefined()
 
 def nullHeuristic(state, problem=None):
@@ -166,7 +171,8 @@ def nullHeuristic(state, problem=None):
 
 def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
-    "*** YOUR CODE HERE ***"
+    from util import PriorityQueue
+    return firstSearchSolution(problem, PriorityQueue(), "astar")
     util.raiseNotDefined()
 
 
